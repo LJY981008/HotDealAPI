feafb9ccb6ffd98d4fa317a4d46c2d97a4984ad2
이준영 <tera199810@gmail.com>
2025-07-08 12:25:17 +0900

    refactor: 도메인 패키지 위치조정

--- src/main/java/com/example/hotdeal/domain/event/infra/EventRepository.java
+++ src/main/java/com/example/hotdeal/domain/event/infra/EventRepository.java
@@ -1,6 +1,6 @@
 package com.example.hotdeal.domain.event.infra;
 
-import com.example.hotdeal.domain.event.domain.Event;
+import com.example.hotdeal.domain.event.domain.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventRepository extends JpaRepository<Event,Long> {
