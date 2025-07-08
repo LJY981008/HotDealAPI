5e9cb12a7ad638d195079aaecf032eea73d1db3b
이준영 <tera199810@gmail.com>
2025-07-08 12:00:18 +0900

    fix : 쓸모없는 임포트 제거

--- src/main/java/com/example/hotdeal/domain/product/product/infra/ProductRepository.java
+++ src/main/java/com/example/hotdeal/domain/product/product/infra/ProductRepository.java
@@ -3,7 +3,5 @@ package com.example.hotdeal.domain.product.product.infra;
 import com.example.hotdeal.domain.product.product.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
 }
