a58abd1ccc215c56d801fb9d08f74b7ca3b7de79
이준영 <tera199810@gmail.com>
2025-07-08 11:57:11 +0900

    feat : 제품에 이벤트 등록 API 추가 제품에 이벤트 등록하는 API 생성 DSL에 작성한 커스텀 쿼리 사용

--- src/main/java/com/example/hotdeal/domain/product/product/api/ProductController.java
+++ src/main/java/com/example/hotdeal/domain/product/product/api/ProductController.java
@@ -1,11 +1,14 @@
 package com.example.hotdeal.domain.product.product.api;
 
 import com.example.hotdeal.domain.product.product.application.ProductService;
+import com.example.hotdeal.domain.product.product.domain.AddEventRequest;
 import com.example.hotdeal.domain.product.product.domain.AddEventResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
@@ -15,11 +18,11 @@ public class ProductController {
     private final ProductService productService;
 
     @PatchMapping("/add-event")
-    public ResponseEntity<AddEventResponse> addEvent(
-            @RequestParam Long productId,
-            @RequestParam Long eventId
+    public ResponseEntity<List<AddEventResponse>> addEvent(
+            @RequestParam Long eventId,
+            @RequestBody AddEventRequest request
     ){
-        AddEventResponse addEventResponse = productService.addEvent(productId, eventId);
-        return ResponseEntity.status(HttpStatus.OK).body(addEventResponse);
+        List<AddEventResponse> addEventResponses = productService.addEvent(request, eventId);
+        return ResponseEntity.status(HttpStatus.OK).body(addEventResponses);
     }
 }
--- src/main/java/com/example/hotdeal/domain/product/product/application/ProductService.java
+++ src/main/java/com/example/hotdeal/domain/product/product/application/ProductService.java
@@ -1,13 +1,15 @@
 package com.example.hotdeal.domain.product.product.application;
 
+import com.example.hotdeal.domain.product.product.domain.AddEventRequest;
 import com.example.hotdeal.domain.product.product.domain.AddEventResponse;
-import com.example.hotdeal.domain.product.product.domain.Product;
 import com.example.hotdeal.domain.product.product.infra.ProductRepository;
 import com.example.hotdeal.global.enums.CustomErrorCode;
 import com.example.hotdeal.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,12 +18,11 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     @Transactional
-    public AddEventResponse addEvent(Long productId, Long eventId) {
-        Product product = productRepository.findById(productId)
-                .orElseThrow(() -> new CustomException(CustomErrorCode.NOT_FOUND_PRODUCT));
-        product.addEvent(eventId);
-
-        Product save = productRepository.save(product);
-        return new AddEventResponse(save);
+    public List<AddEventResponse> addEvent(AddEventRequest request, Long eventId) {
+        long updatedCount = productRepository.updateProductEventIds(request.getProductIds(), eventId);
+        if (updatedCount <= 0) {
+            throw new CustomException(CustomErrorCode.NOT_FOUND_PRODUCT);
+        }
+        return productRepository.findProductsByIds(request.getProductIds());
     }
 }
