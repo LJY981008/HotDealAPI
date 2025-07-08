34447617773a7a8ce3dde3d228067c75ab549d51
이준영 <tera199810@gmail.com>
2025-07-08 11:55:12 +0900

    feat : DSL사용 제품에 이벤트 등록 및 조회 기능 작성

--- src/main/java/com/example/hotdeal/domain/product/product/infra/ProductRepository.java
+++ src/main/java/com/example/hotdeal/domain/product/product/infra/ProductRepository.java
@@ -3,5 +3,7 @@ package com.example.hotdeal.domain.product.product.infra;
 import com.example.hotdeal.domain.product.product.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
 }
