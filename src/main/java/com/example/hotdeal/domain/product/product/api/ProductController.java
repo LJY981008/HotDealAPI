e0b62553e528b2a1080a7cb0f8cb4c9df06acea9
이준영 <tera199810@gmail.com>
2025-07-08 16:56:07 +0900

    feat: 프로덕트 정보 조회를 위한 API 작성 프로덕트 Id List를 통한 조회Api 작성

--- src/main/java/com/example/hotdeal/domain/product/product/api/ProductController.java
+++ src/main/java/com/example/hotdeal/domain/product/product/api/ProductController.java
@@ -1,10 +1,28 @@
 package com.example.hotdeal.domain.product.product.api;
 
+import com.example.hotdeal.domain.product.product.application.ProductService;
+import com.example.hotdeal.domain.product.product.domain.SearchProductListRequest;
+import com.example.hotdeal.domain.product.product.domain.SearchProductResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
 @RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/search-product")
+    public ResponseEntity<List<SearchProductResponse>> getProduct(
+            @Valid @RequestBody SearchProductListRequest searchProductListRequest
+    ){
+        List<SearchProductResponse> searchProductResponse = productService.getProduct(searchProductListRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(searchProductResponse);
+    }
 }
--- src/main/java/com/example/hotdeal/domain/product/product/infra/ProductRepository.java
+++ src/main/java/com/example/hotdeal/domain/product/product/infra/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.example.hotdeal.domain.product.product.infra;
 
-import com.example.hotdeal.domain.product.product.domain.Product;
+
+import com.example.hotdeal.domain.product.product.domain.command.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+    List<Product> findByProductIdIn(List<Long> productIds);
 }
