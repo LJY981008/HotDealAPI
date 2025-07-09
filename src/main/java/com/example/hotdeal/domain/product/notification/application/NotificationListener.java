64212597a22507e9b94e2c9a747bc125baab88d1
이준영 <tera199810@gmail.com>
2025-07-09 11:53:22 +0900

    feat : 이벤트 리스너에서 서비스 호출 서비스 호출 및 데이터 저장

---
+++ src/main/java/com/example/hotdeal/domain/product/notification/application/NotificationListener.java
@@ -0,0 +1,34 @@
+package com.example.hotdeal.domain.product.notification.application;
+
+import com.example.hotdeal.domain.event.domain.dto.WSEventProduct;
+import com.example.hotdeal.domain.product.notification.domain.ListenProductEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class NotificationListener {
+
+    private final NotificationService notificationService;
+
+    @EventListener
+    public void addProductDiscountEvent(List<WSEventProduct> events){
+        try {
+            log.info("addProductDiscountEvent 시작");
+
+            List<ListenProductEvent> listenProductEvents =
+                    events.stream().map(ListenProductEvent::new).toList();
+
+            notificationService.notifyProductEventMessage(listenProductEvents);
+
+            log.info("addProductDiscountEvent 종료");
+        } catch (Exception e) {
+            log.error("addProductDiscountEvent 처리 실패 message : {}", e.getMessage());
+        }
+    }
+}
--- src/main/java/com/example/hotdeal/domain/product/notification/application/NotificationService.java
+++ src/main/java/com/example/hotdeal/domain/product/notification/application/NotificationService.java
@@ -1,4 +1,23 @@
 package com.example.hotdeal.domain.product.notification.application;
 
+import com.example.hotdeal.domain.product.notification.domain.ListenProductEvent;
+import com.example.hotdeal.domain.product.notification.domain.Notification;
+import com.example.hotdeal.domain.product.notification.infra.NotificationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+
+
+    public void notifyProductEventMessage(List<ListenProductEvent> listenProductEvents) {
+        List<Notification> notifications = listenProductEvents.stream().map(ListenProductEvent::toNotification).toList();
+        notificationRepository.saveAll(notifications);
+    }
 }
