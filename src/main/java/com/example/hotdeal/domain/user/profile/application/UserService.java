04d20c82dc5341cba7d6f45d140ef0220b5becb0
teopteop <teopteop0718@gmail.com>
2025-07-10 23:09:41 +0900

    feat: UserService 초기세팅

--- src/main/java/com/example/hotdeal/domain/user/profile/application/UserService.java
+++ src/main/java/com/example/hotdeal/domain/user/profile/application/UserService.java
@@ -1,6 +1,13 @@
 package com.example.hotdeal.domain.user.profile.application;
 
-public class UserService {
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true) //읽기 전용 서비스
+public class UserService {
 
 }
