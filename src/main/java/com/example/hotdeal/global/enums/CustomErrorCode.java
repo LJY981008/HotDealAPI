f0bb7153d774fa30e812dabdb0377c90e49d611d
teopteop <teopteop0718@gmail.com>
2025-07-10 19:37:30 +0900

    feat: 커스텀 예외 추가

--- src/main/java/com/example/hotdeal/global/enums/CustomErrorCode.java
+++ src/main/java/com/example/hotdeal/global/enums/CustomErrorCode.java
@@ -12,6 +12,7 @@ public enum CustomErrorCode {
     NOT_FOUND_PRODUCT(HttpStatus.NOT_FOUND, "제품이 없습니다."),
     NOT_FOUND_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 유효하지 않거나 만료된 토큰입니다."),
     TOKEN_MISMATCH(HttpStatus.UNAUTHORIZED, "저장된 토큰과 일치하지 않습니다."),
+    PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
     ;
 
     private final HttpStatus httpStatus;
