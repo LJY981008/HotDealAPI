aa34091b614b787e62635561cd51a4aacff11864
teopteop <teopteop0718@gmail.com>
2025-07-10 23:06:51 +0900

    feat: 이벤트 추가
    Auth 추가, 삭제, 복구 이벤트

---
+++ src/main/java/com/example/hotdeal/domain/user/auth/event/UserWithdrawnEvent.java
@@ -0,0 +1,10 @@
+package com.example.hotdeal.domain.user.auth.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(staticName = "of")
+public class UserWithdrawnEvent {
+	private final Long userId;
+}
