2c4e036905744f84a5f87e9fe78257815ceb19f3
teopteop <teopteop0718@gmail.com>
2025-07-11 11:13:13 +0900

    refactor: .yml 수정, 환경변수에 맞게 RedisConfig 수정
    REDIS_HOST=127.0.0.1
    REDIS_PORT=6379
    REDIS_PASSWORD=로컬비밀번호

--- src/main/java/com/example/hotdeal/global/infrastructure/cache/RedisConfig.java
+++ src/main/java/com/example/hotdeal/global/infrastructure/cache/RedisConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -11,15 +13,27 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-	@Value("${spring.data.redis.port}")
+	@Value("${redis.port}")
 	private int port;
 
-	@Value("${spring.data.redis.host}")
+	@Value("${redis.host}")
 	private String host;
+
+	@Value("${redis.password:}")
+	private String password;
 
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory(host, port);
+		RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+		config.setHostName(host);
+		config.setPort(port);
+
+		//비밀번호 있을시 세팅
+		if(!password.isBlank()){
+			config.setPassword(RedisPassword.of(password));
+		}
+
+		return new LettuceConnectionFactory(config);
 	}
 
 	@Bean
