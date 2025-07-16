36b97cbd5f621437ecfe1dec8e7551bd9a39146d
이준영 <tera199810@gmail.com>
2025-07-16 17:46:32 +0900

    test: test

--- src/main/java/com/example/hotdeal/HotDealApplication.java
+++ src/main/java/com/example/hotdeal/HotDealApplication.java
@@ -3,17 +3,13 @@ package com.example.hotdeal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
-@EnableAsync
 @SpringBootApplication
-@EnableScheduling
 public class HotDealApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HotDealApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HotDealApplication.class, args);
+    }
 
 }
